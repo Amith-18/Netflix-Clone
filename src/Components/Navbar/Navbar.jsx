ab33da254292ec37458a22459f928014f5cbc334
Amith-18 <amithsanil@outlook.com>
2024-12-21 21:15:19 +0400

    Added ul to navbar-right

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import './Navbar.css'
+import logo from '../../assets/logo.png'
 
 const Navbar = () => {
   return (
     <div className='navbar'>
-        <div className="navbar-left"></div>
+        <div className="navbar-left">
+            <img src={logo} alt="" />
+            <ul>
+                <li>Home</li>
+                <li>TV Shows</li>
+                <li>Movies</li>
+                <li>New & Popular</li>
+                <li>My List</li>
+                <li>Browse by Language</li>
+            </ul>
+        </div>
         <div className="navbar-right"></div>
     </div>
   )
