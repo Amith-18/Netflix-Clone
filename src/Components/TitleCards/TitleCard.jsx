91b60ae6ac00973003c36395eac18d289c8dd4a9
Amith-18 <amithsanil@outlook.com>
2024-12-21 21:06:18 +0400

    Added New Files

---
+++ src/Components/TitleCards/TitleCard.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './TitleCard.css'
+
+const TitleCard = () => {
+    return (
+        <div className='title'>
+
+        </div>
+    )
+}
+
+export default TitleCard
