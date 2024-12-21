c75f7b2d4855ce1de45c7421fe6b18879bdec904
Amith-18 <amithsanil@outlook.com>
2024-12-21 21:23:29 +0400

    Added img tag to navbar-right

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,24 +1,37 @@
 import React from 'react'
 import './Navbar.css'
 import logo from '../../assets/logo.png'
+import Search_icon from '../../assets/search_icon.svg'
+import bell_icon from '../../assets/bell_icon.svg'
+import profile_img from '../../assets/profile_img.png'
+import bell_icon from '../../assets/bell_icon.svg'
+
 
 const Navbar = () => {
-  return (
-    <div className='navbar'>
-        <div className="navbar-left">
-            <img src={logo} alt="" />
-            <ul>
-                <li>Home</li>
-                <li>TV Shows</li>
-                <li>Movies</li>
-                <li>New & Popular</li>
-                <li>My List</li>
-                <li>Browse by Language</li>
-            </ul>
+    return (
+        <div className='navbar'>
+            <div className="navbar-left">
+                <img src={logo} alt="" />
+                <ul>
+                    <li>Home</li>
+                    <li>TV Shows</li>
+                    <li>Movies</li>
+                    <li>New & Popular</li>
+                    <li>My List</li>
+                    <li>Browse by Language</li>
+                </ul>
+            </div>
+            <div className="navbar-right">
+                <img className='icons' src={Search_icon} alt="" />
+                <p>Children</p>
+                <img className='icons' src={bell_icon} alt="" />
+                <div className="navbar-profile">
+                    <img className='icons' src={profile_img} alt="" />
+                    <img className='icons' src={bell_icon} alt="" />
+                </div>
+            </div>
         </div>
-        <div className="navbar-right"></div>
-    </div>
-  )
+    )
 }
 
 export default Navbar
