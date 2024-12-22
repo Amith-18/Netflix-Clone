67c31f4b2976fa2ed835d409e0c0e5a9ef1dd5e5
Amith-18 <amithsanil@outlook.com>
2024-12-22 21:35:41 +0400

    Added div in home.jsx

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
 import './Home.css'
 import Navbar from '../../Components/Navbar/Navbar'
+import hero_banner from '../../assets/hero_banner.jpg'
+import hero_title from '../../assets/hero_title.png'
 
 const Home = () => {
   return (
     <div className='home'>
         <Navbar/>
+        <div className="hero">
+          <img src={hero_banner} alt="" className='banner-img' />
+          <div className="hero-caption">
+            <img src={hero_title} alt="" className='caption-img' />
+            <p>"The Protector" is a Turkish drama series about Hakan, a young man in Istanbul, who discovers his connection to an ancient secret order tasked with protecting the city from an immortal enemy.</p>
+          </div>
+        </div>
     </div>
   )
 }
