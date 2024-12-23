48976b17b03034a3072df17dddeaac0dcfc45fa5
Amith-18 <amithsanil@outlook.com>
2024-12-23 20:46:13 +0400

    added footer.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Home from './Pages/Home/Home'
 import TitleCard from './Components/TitleCards/TitleCard'
+import Footer from './Components/Footer/Footer'
 
 const App = () => {
   return (
